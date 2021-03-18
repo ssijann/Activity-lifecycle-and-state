a4e9363bf15c2cd4f8080b637d5a0af7718324ff
ssijann <rockstarsijan@gmail.com>
2021-03-18 19:35:58 +0545

    codingchallenge

--- app/src/main/java/com/example/lifecycleandstate/SecondActivity.java
+++ app/src/main/java/com/example/lifecycleandstate/SecondActivity.java
@@ -1,39 +1,60 @@
 package com.example.lifecycleandstate;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_ITEMS = "me.mahakagg.shoppinglist.extra.ITEMS";
 
-    public static final String EXTRA_REPLY =
-            "com.example.android.twoactivities.extra.REPLY";
-
-    private EditText mReply;
-    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(com.example.lifecycleandstate.MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
-        mReply = findViewById(R.id.editText_second);
+//        Intent intent = getIntent();
     }
 
-    public void returnReply(View view) {
-        Log.d(LOG_TAG, "End SecondActivity");
-        String reply = mReply.getText().toString();
-        Intent replyIntent = new Intent();
-        replyIntent.putExtra(EXTRA_REPLY, reply);
-        setResult(RESULT_OK,replyIntent);
+    public void selectItem(View view) {
+        Intent return_items_intent = new Intent();
 
-        finish();
+        switch (view.getId()){
+            case R.id.button_Chocolate:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Chocolate"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_oil:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Oil"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_milk:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Milk"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_eggs:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Eggs"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_cheese:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Cheese"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_rice:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Rice"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_wheat:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Wheat Flour"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_bread:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Bread"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_sugar:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Sugar"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            case R.id.button_salt:
+                return_items_intent.putExtra(EXTRA_ITEMS, "Salt"); setResult(RESULT_OK, return_items_intent); finish();
+                break;
+            default:
+                Toast.makeText(SecondActivity.this, "There was an error", Toast.LENGTH_LONG).show();
+                break;
+        }
+
     }
 }
